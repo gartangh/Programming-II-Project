770cdac247c57057894936d4893a38709666141b
gilles ballegeer <gilles.ballegeer@ugent.be>
2017-04-09 22:01:16 +0200

    added allsystems

---
+++ SlimeVolley/allSystems.h
@@ -0,0 +1,18 @@
+#ifndef ALLSYSTEMS_H
+#define ALLSYSTEMS_H
+
+#include"system.h"
+#include "system_ai.h"
+#include "system_collision.h"
+#include "system_eyes.h"
+#include "system_input_multi.h"
+#include "system_input_single.h"
+#include "system_motion.h"
+#include "system_output.h"
+#include "system_points.h"
+#include "system_render.h"
+#include "system_replay.h"
+#include "system_state_multi.h"
+#include "system_state_single.h"
+
+#endif
